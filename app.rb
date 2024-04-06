92a8e10aade131289ca710e2cc0f8e3d2a9fe0e6
Sarat1902 <161099567+Sarat1902@users.noreply.github.com>
2024-04-06 12:49:31 +0000

    commit

--- app.rb
+++ app.rb
@@ -10,7 +10,46 @@ end
 
 get("/rock") do
   move = ["rock","paper","scissors"]
-  moves = move.sample
-  outcome = "tied"
+  @moves = move.sample
+  
+  if @moves == "rock"
+    @outcome = "tied"
+  elsif @moves == "paper"
+    @outcome = "You Lose"
+  else
+    @outcome = "You Win!!"
+  end
+
   erb(:rock)
+end
+
+
+get("/paper") do
+  move = ["rock","paper","scissors"]
+  @moves = move.sample
+  
+  if @moves == "rock"
+    @outcome = "You Win!!"
+  elsif @moves == "paper"
+    @outcome = "You Tied"
+  else
+    @outcome = "You Lost"
+  end
+
+  erb(:paper)
+end
+
+get("/scissors") do
+  move = ["rock","paper","scissors"]
+  @moves = move.sample
+  
+  if @moves == "rock"
+    @outcome = "You Lost"
+  elsif @moves == "paper"
+    @outcome = "You Win!!"
+  else
+    @outcome = "You Tied"
+  end
+
+  erb(:scissors)
 end
